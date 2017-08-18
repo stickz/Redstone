bf6629d99bf8ffdc7dc15560db20255798be7db4
stickz <stickman002@mail.com>
2017-08-18 16:09:12 -0400

    Oops, use proper syntax for curl

--- build_scripts/build.sh
+++ build_scripts/build.sh
@@ -140,7 +140,7 @@ if [ ! -d "$SOURCEMOD_DIR" ] || [ "$cache" = false ]; then
     $SOURCEMOD_ARCHIVE_URL to $SOURCEMOD_DIR"
   fi
   mkdir -p $SOURCEMOD_DIR
-  curl -# $SOURCEMOD_ARCHIVE_URL
+  curl -O $SOURCEMOD_ARCHIVE_URL
 
   if [ "$OS" = "Mac" ]; then
     # -o stands for OVERWRITE, meaning don't prompt if overwriting files
@@ -163,7 +163,7 @@ if [ ! -e "$UPDATER_PATCH_PATH" ] || [ "$cache" = false ]; then
   if [ "$verbose" = true ]; then
     echo "- Downloading updater patch from $UPDATER_PATCH_URL"
   fi
-  curl -# $UPDATER_PATCH_URL
+  curl -O $UPDATER_PATCH_URL
 else
   if [ "$verbose" = true ]; then
     echo "- Using previously downloaded updater patch"
