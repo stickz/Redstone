stickz/Redstone

commit 23a452eaee601c9cb9f7947745b47c8a04892fe2

Improve Bot Features (#542)
- Remove turret count bot quota reduction. This is no longer required.

- Subtract the spectator count from the max bot count, to keep connection slots open.

- Set bot disable & reduction values during map change or convar change only; not always.

- Simplify the bot modulus quota functions. Move them to the main file due to small size.

- Refactor some code for future improvements. Cleanup some unnecessary lines.

- Create new nd_stocks functions to handle powers and modulus tasks.

addons/sourcemod/scripting/nd_bot_features.sp
@@ -22,14 +22,11 @@
 #include <nd_balancer>
 #include <nd_rounds>
 #include <nd_maps>
-#include <nd_turret_eng>
 #include <nd_commands>
 #include <nd_spec>
 
+// Include the file which stores config values
 #include "nd_bot_feat/convars.sp"
-//functions required to create a modulous bot quota
-//simply calling getBotModulusQuota() will return the integer
-#include "nd_bot_feat/modulus_quota.sp"
 
 bool disableBots = false;
 float timerDuration = 1.5;
@@ -57,7 +54,14 @@ public void OnPluginStart()
 	AddUpdaterLibrary(); //auto-updater
 }
 
-public void OnMapEnd() {
+public void OnMapStart() 
+{
+	SetBotDisableValues(); // convars.sp
+	SetBotReductionValues(); // convars.sp
+}
+
+public void OnMapEnd() 
+{
 	disableBots = false;
 	SignalMapChange();	
 }
@@ -138,15 +142,16 @@ void checkCount()
 		else if (GDSC_AVAILABLE())
 		{	
 			// If one team has less players than the other
-			int teamLessPlys = getTeamLessPlayers();			
-			if (teamLessPlys != TEAM_NONE)
-			{
-				int posOverBalance = getPositiveOverBalance(); // The player difference between the two teams				
-				int dynamicSlots = GetDynamicSlotCount() - 2; // Get the bot count to fill empty team slots
-				quota = getBotFillerQuota(posOverBalance);
+			int posOverBalance = getPositiveOverBalance();	
+			if (posOverBalance >= 1)
+			{				
+				quota = getBotFillerQuota(posOverBalance); // Get number of bots to fill
 				
-				toggleBooster(quota >= dynamicSlots && posOverBalance >= 2);
-				CreateTimer(timerDuration, TIMER_CheckAndSwitchFiller, teamLessPlys, TIMER_FLAG_NO_MAPCHANGE);	
+				// Boost server slots if quota extends cap and team difference is 2+
+				toggleBooster(quota >= GetDynamicSlotCount()-2 && posOverBalance >= 2);
+				
+				// Create a timer after envoking bot quota, to switch bots to the fill team
+				CreateTimer(timerDuration, TIMER_CheckAndSwitchFiller, _, TIMER_FLAG_NO_MAPCHANGE);	
 			}
 			else { quota = 0; } // Otherwise, set filler quota to 0
 		}
@@ -174,13 +179,6 @@ void InitializeServerBots()
 	
 	ServerCommand("bot_quota %d", quota);
 	ServerCommand("mp_limitteams %d", g_cvar[RegOverblance].IntValue);
-}
-
-bool boostBots()
-{
-	bool boost = g_cvar[BoostBots].BoolValue;
-	toggleBooster(boost);
-	return boost;
 }
 
 //Turn 32 slots on or off for bot quota
@@ -211,33 +209,66 @@ void SignalMapChange()
 	ServerCommand("mp_limitteams 1");
 }
 
-//When teams have two or more less players
 int getBotFillerQuota(int plyDiff)
 {
 	// Set bot count to player count difference * x - 1.
 	// Team count offset required to fill the quota properly.
-	int total = OnTeamCount() + GetBotCountByPow(float(plyDiff), g_cvar[BotDiffMult].FloatValue);
+	int total = OnTeamCount() + RoundPowToNearest(float(plyDiff), g_cvar[BotDiffMult].FloatValue);
 	
 	// Add the spectator count becuase it takes away one bot by default
-	total += ValidTeamCount(TEAM_SPEC);
+	int specCount = ValidTeamCount(TEAM_SPEC);
+	total += specCount;
 	
 	// Set a ceiling to be returned, leave two connecting slots
-	int max = 30 - ValidTeamCount(TEAM_UNASSIGNED);
+	int max = 30 - ValidTeamCount(TEAM_UNASSIGNED) - specCount;
 	return total > max ? max : total;
 }
 
-int GetBotCountByPow(float diff, float exp) {
-	return RoundToNearest(Pow(diff, exp));
+int getBotModulusQuota()
+{	
+	// Get max quota and the current spectator count
+	int maxQuota = g_cvar[BoosterQuota].IntValue;
+	int specCount = ValidTeamCount(TEAM_SPEC);
+	
+	// Caculate the value for the bot cvar. Adjust bot value to offset the spectators
+	int botAmount = maxQuota - botReductionValue + specCount;
+	
+	// If the bot value is greater than max, we must use the max instead
+	int totalCount = maxQuota - specCount - ValidTeamCount(TEAM_UNASSIGNED);
+	if (botAmount >= totalCount)
+		botAmount = totalCount;
+	
+	// If required, modulate the bot count so the number is even
+	return GetNumEvenM1(botAmount);
 }
 
-public Action TIMER_CheckAndSwitchFiller(Handle timer, any team)
+bool boostBots()
 {
-	CheckAndSwitchFiller(team);
+	bool boost = g_cvar[BoostBots].BoolValue;
+	toggleBooster(boost);
+	return boost;
+}
+
+bool CheckShutOffBots()
+{	
+	// Get the empire, consort and total on team count
+	int empireCount = RED_GetTeamCount(TEAM_EMPIRE);
+	int consortCount = RED_GetTeamCount(TEAM_CONSORT);	
+	
+	// If total count on one or both teams is reached, disable bots
+	bool isTotalDisable = (empireCount + consortCount) >= totalDisable;
+	return isTotalDisable || empireCount >= teamDisable || consortCount >= teamDisable;
+}
+
+public Action TIMER_CheckAndSwitchFiller(Handle timer)
+{
+	CheckAndSwitchFiller();
 	return Plugin_Handled;
 }
 
-void CheckAndSwitchFiller(int teamLessPlys)
+void CheckAndSwitchFiller()
 {
+	int teamLessPlys = getTeamLessPlayers();	
 	for (int bot = 1; bot < MaxClients; bot++)
 	{
 		if (IsClientConnected(bot) && IsClientInGame(bot) && IsFakeClient(bot) && GetClientTeam(bot) != teamLessPlys)
@@ -250,8 +281,6 @@ void CheckAndSwitchFiller(int teamLessPlys)
 
 public APLRes AskPluginLoad2(Handle myself, bool late, char[] error, int err_max)
 {
-	MarkNativeAsOptional("ND_GetTurretCount");
-	MarkNativeAsOptional("ND_GetTeamTurretCount");
 	MarkNativeAsOptional("ND_PlayerSpecLocked");
 	RegPluginLibrary("afkmanager");
 }

addons/sourcemod/scripting/include/nd_stocks.inc
@@ -212,6 +212,18 @@ stock int getPositiveOverBalance()
 	return overBalance;
 }
 
+stock int GetNumEvenP1(int num) {
+	return num % 2 == 0 ? num : num + 1;
+}
+
+stock int GetNumEvenM1(int num) {
+	return num % 2 == 0 ? num : num - 1;
+}
+
+stock int RoundPowToNearest(float base, float exp) {
+	return RoundToNearest(Pow(base, exp));
+}
+
 /* Team Constants */
 stock int getOtherTeam(int team)
 {

addons/sourcemod/scripting/nd_bot_feat/convars.sp
@@ -17,14 +17,14 @@ enum convars
 	 ConVar:DisableBotsTeamInc,
 	 
 	 ConVar:BotOverblance,
-	 ConVar:RegOverblance,
-	 ConVar:turretCountDec,
-	 ConVar:turretBotDec
+	 ConVar:RegOverblance
 };
 
 ConVar g_cvar[convars];
 
 bool visibleBoosted = false;
+int totalDisable, teamDisable;
+int botReductionValue;
 
 void CreatePluginConvars()
 {
@@ -46,8 +46,6 @@ void CreatePluginConvars()
 	
 	g_cvar[BotOverblance] = AutoExecConfig_CreateConVar("sm_bot_overbalance", "3", "sets team difference allowed with bots enabled"); 
 	g_cvar[RegOverblance] = AutoExecConfig_CreateConVar("sm_reg_overbalance", "1", "sets team difference allowed with bots disabled");
-	g_cvar[turretCountDec] = AutoExecConfig_CreateConVar("sm_bot_turrent", "40", "sets number of turrets to reduce bot counts");
-	g_cvar[turretBotDec] = AutoExecConfig_CreateConVar("sm_bot_quota_turret", "14", "sets max bot quota with turrets enabled");
 	
 	HookConVarChange(g_cvar[BoostBots], OnBotBoostChange);
 	
@@ -65,4 +63,45 @@ public void OnBotBoostChange(ConVar convar, char[] oldValue, char[] newValue)
 			visibleBoosted = convar.BoolValue;
 		}
 	}
+	
+	SetBotDisableValues();
+	SetBotReductionValues();
+}
+
+void SetBotDisableValues()
+{
+	// Get the current map we're playing
+	char map[32];
+	GetCurrentMap(map, sizeof(map));
+
+	// Disable bots sooner if it's a tiny maps
+	if (ND_CustomMapEquals(map, ND_Sandbrick))
+	{		
+		teamDisable = g_cvar[DisableBotsTeamDec].IntValue;
+		totalDisable = g_cvar[DisableBotsAtDec].IntValue;
+	}
+	
+	// Disable bots later on big maps, to compensate for the size
+	else if (ND_StockMapEquals(map, ND_Gate) || ND_StockMapEquals(map, ND_Downtown))
+	{
+		teamDisable = g_cvar[DisableBotsTeamInc].IntValue;
+		totalDisable = g_cvar[DisableBotsAtInc].IntValue;	
+	}
+	
+	else
+	{
+		teamDisable = g_cvar[DisableBotsTeam].IntValue;
+		totalDisable = g_cvar[DisableBotsAt].IntValue
+	}	
+}
+
+void SetBotReductionValues()
+{
+	// Get the current map we're playing
+	char map[32];
+	GetCurrentMap(map, sizeof(map));
+	
+	// If small map, reduce the mnumber of bots. Otherwise, use the regular bot count
+	bool smallMap = ND_CustomMapEquals(map, ND_Sandbrick) || ND_CustomMapEquals(map, ND_Mars);
+	botReductionValue = smallMap ? g_cvar[BotReductionDec].IntValue : g_cvar[BotReduction].IntValue;
 }

addons/sourcemod/scripting/nd_bot_feat/modulus_quota.sp
@@ -1,114 +1,0 @@
-/* Functions for Bot Modulus Quota */
-int getBotModulusQuota()
-{
-	int specCount = getSpectatorAdjustment();
-	int toSubtract = getUnassignedAdjustment();	
-
-	int totalCount = g_cvar[BoosterQuota].IntValue - specCount - toSubtract;	
-	
-	int reducedCount = GetBotReductionCount();
-	totalCount = GetSmallMapCount(totalCount, specCount, reducedCount);		
-	
-	return totalCount;
-}
-
-int getSpectatorAdjustment() {
-	return ValidTeamCount(TEAM_SPEC) % 2 == 0 ? 2 : 1;
-}
-
-int getUnassignedAdjustment() //Fix bug which prevents connecting to the server
-{	
-	int NotAssignedCount = ValidTeamCount(TEAM_UNASSIGNED);	
-	
-	switch (NotAssignedCount)
-	{
-		case 0,1: NotAssignedCount = 0;
-		case 2,3: NotAssignedCount = 2;
-		default: NotAssignedCount = 4;	
-	}		
-	
-	return NotAssignedCount;
-}
-
-/* List the really tinny maps to reduce further, (assume default if unlisted) */
-int GetBotReductionCount()
-{
-	char map[32];
-	GetCurrentMap(map, sizeof(map));
-	
-	if (ND_CustomMapEquals(map, ND_Sandbrick) || ND_CustomMapEquals(map, ND_Mars))
-		return g_cvar[BotReductionDec].IntValue;
-
-	return g_cvar[BotReduction].IntValue;
-}
-
-/* Gets the max number of bots, based on the number of turrets on the map */
-int GetTurretMaxQuota() {
-	return ND_TurretCount() >= g_cvar[turretCountDec].IntValue ? g_cvar[turretBotDec].IntValue
-				   				   : g_cvar[BoosterQuota].IntValue;
-}
-
-/* Get the number of bots after the reduction */
-int GetSmallMapCount(int totalCount, int specCount, int rQuota)
-{
-	// Get max quota and reduce amount
-	int maxQuota = g_cvar[BoosterQuota].IntValue;
-
-	// Caculate the value for the bot cvar
-	int botAmount = totalCount - rQuota + (maxQuota - totalCount);
-	
-	// Adjust bot value to offset the spectators 
-	botAmount += specCount;
-	
-	// If the bot value is greater than max, we must use the max instead
-	if (botAmount >= totalCount)
-		botAmount = totalCount;
-	
-	// If the bot value is greater the amount allocated for turrets,
-	// We must use the max turret allocation instead
-	int maxTurretQuota = GetTurretMaxQuota();
-	if (botAmount >= maxTurretQuota)
-		botAmount = maxTurretQuota;
-					
-	// If required, modulate the bot count so the number is even
-	if (botAmount % 2 != totalCount % 2)
-		return botAmount - 1;
-
-	return botAmount;
-}
-
-bool CheckShutOffBots()
-{
-	// Get the current map we're playing
-	char map[32];
-	GetCurrentMap(map, sizeof(map));
-	
-	// Get the empire, consort and total on team count
-	int empireCount = RED_GetTeamCount(TEAM_EMPIRE);
-	int consortCount = RED_GetTeamCount(TEAM_CONSORT);	
-	int totalDisable, teamDisable;
-	
-	// Disable bots sooner if it's a tiny maps
-	if (ND_CustomMapEquals(map, ND_Sandbrick))
-	{		
-		teamDisable = g_cvar[DisableBotsTeamDec].IntValue;
-		totalDisable = g_cvar[DisableBotsAtDec].IntValue;
-	}
-	
-	// Disable bots later on big maps, to compensate for the size
-	else if (ND_StockMapEquals(map, ND_Gate) || ND_StockMapEquals(map, ND_Downtown))
-	{
-		teamDisable = g_cvar[DisableBotsTeamInc].IntValue;
-		totalDisable = g_cvar[DisableBotsAtInc].IntValue;	
-	}
-	
-	else
-	{
-		teamDisable = g_cvar[DisableBotsTeam].IntValue;
-		totalDisable = g_cvar[DisableBotsAt].IntValue
-	}
-	
-	// If total count on one or both teams is reached, disable bots
-	bool isTotalDisable = (empireCount + consortCount) >= totalDisable;
-	return isTotalDisable || empireCount >= teamDisable || consortCount >= teamDisable;
-}

commit 5cc827f082b27dbdc22f57ac39d771d88b886000

Fix Bot Features
Fix the modulus value for modulus quota. Regression from #542.

addons/sourcemod/scripting/nd_bot_features.sp
@@ -243,9 +243,9 @@ int getBotModulusQuota()
 	int totalCount = maxQuota - specCount - ValidTeamCount(TEAM_UNASSIGNED);
 	if (botAmount >= totalCount)
 		botAmount = totalCount;
-	
-	// If required, modulate the bot count so the number is even
-	return GetNumEvenM1(botAmount);
+		
+	// If required, modulate the bot count so the number is even on the scoreboard
+	return botAmount % 2 != totalCount % 2 ? GetNumEvenM1(botAmount) : botAmount;
 }
 
 bool boostBots()